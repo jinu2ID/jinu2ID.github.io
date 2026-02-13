a0ca8ee6e4a90d2685a55ff73f407ebcb8024595
jinu2ID <jinu.jacob90@gmail.com>
2026-02-12 22:54:44 -0500

    Add scroll animation

---
+++ js/scroll-animation.js
@@ -0,0 +1,20 @@
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  },
+);
+
+fadeElements.forEach(function (element) {
+  observer.observe(element);
+});
